971976497c81e3b08663d3d72e411be9899b22ca
Suresh S <suthamani51@gmail.com>
2025-01-31 19:17:54 +0530

    corrected profile card

--- lib/widgets/ProfileCard.dart
+++ lib/widgets/ProfileCard.dart
@@ -8,58 +8,67 @@ class ProfileCard extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
     return Center(
-      child: Container(
-        width: 400,
-        decoration: BoxDecoration(
-          borderRadius: BorderRadius.circular(14),
-          color: Colors.white,
-        ),
-        child: Column(
-          mainAxisSize: MainAxisSize.min,
-          children: [
-            const SizedBox(height: 16),
-            CircleAvatar(
-              radius: 50,
-              backgroundImage: AssetImage('assets/user.jpg'),
-            ),
-            const SizedBox(height: 10),
-            Text(
-              'Claudia Foster',
-              style: GoogleFonts.manrope(
-                fontSize: 18,
-                fontWeight: FontWeight.bold,
-                color: Colors.black,
+      child: Column(
+        mainAxisSize: MainAxisSize.min,
+        children: [
+          Container(
+            width: 380,
+            decoration: BoxDecoration(
+              borderRadius: BorderRadius.only(
+                topLeft: Radius.circular(16),
+                topRight: Radius.circular(16),
               ),
+              color: Color(0xffffffff),
+              
             ),
-            Text(
-              'Chemist',
-              style: GoogleFonts.manrope(
-                fontSize: 12,
-                color: Color(0xff6b7280),
-              ),
-            ),
-            const SizedBox(height: 10),
-            const Divider(height: 1, color: Colors.grey),
-            Container(
-              padding: const EdgeInsets.symmetric(vertical: 12),
-              decoration: BoxDecoration(
-                color: const Color(0xffeff4fa),
-                borderRadius: const BorderRadius.vertical(
-                  bottom: Radius.circular(16),
+            child: Column(
+              children: [
+                const SizedBox(height: 16),
+                CircleAvatar(
+                  radius: 50,
+                  backgroundImage: AssetImage('assets/user.jpg'),
                 ),
-              ),
-              child: Row(
-                mainAxisAlignment: MainAxisAlignment.center,
-                children: [
-                  _socialIcon('assets/svg/facebook.svg'),
-                  _socialIcon('assets/svg/instagram.svg'),
-                  _socialIcon('assets/svg/github.svg'),
-                  _socialIcon('assets/svg/twitter.svg'),
-                ],
+                const SizedBox(height: 10),
+                Text(
+                  'Claudia Foster',
+                  style: GoogleFonts.manrope(
+                    fontSize: 18,
+                    fontWeight: FontWeight.bold,
+                    color: Colors.black,
+                  ),
+                ),
+                Text(
+                  'Chemist',
+                  style: GoogleFonts.manrope(
+                    fontSize: 12,
+                    color: Color(0xff6b7280),
+                  ),
+                ),
+                const SizedBox(height: 10),
+              ],
+            ),
+          ),
+          Container(
+            width: 380,
+            padding: const EdgeInsets.symmetric(vertical: 12),
+            decoration: BoxDecoration(
+              color: const Color(0xffeff4fa),
+              borderRadius: const BorderRadius.only(
+                bottomLeft: Radius.circular(16),
+                bottomRight: Radius.circular(16),
               ),
             ),
-          ],
-        ),
+            child: Row(
+              mainAxisAlignment: MainAxisAlignment.center,
+              children: [
+                _socialIcon('assets/svg/facebook.svg'),
+                _socialIcon('assets/svg/instagram.svg'),
+                _socialIcon('assets/svg/github.svg'),
+                _socialIcon('assets/svg/twitter.svg'),
+              ],
+            ),
+          ),
+        ],
       ),
     );
   }
